036c19cdde185b7cd8317299652a8dd64a7df0a5
riqsans <thrq88@gmail.com>
2024-06-14 14:59:56 +0700

    menuliskan int main program

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -113,4 +113,56 @@ public:
     }
 };
 
+int main() {
+    BinaryTree obj;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. perfom inorder traversal" << endl;
+        cout << "3. perfom preorder traversal" << endl;
+        cout << "4. perfom postorder traversal" << endl;
+        cout << "5. exit" << endl
+        cout << "Enter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word :";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+        case '3':
+        {
+            obj.preorder(obj.ROOT);
+            break;
+        }
+        case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
+        case '5':
+            return 0;
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+}
+
  
