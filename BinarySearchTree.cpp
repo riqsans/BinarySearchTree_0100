ea44ecb512947387b8747ec0d2be5936f980cfa2
riqsans <thrq88@gmail.com>
2024-06-14 14:38:38 +0700

    menuliskan void inorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -68,6 +68,20 @@ public:
         }
     }
 
+    void inorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is Empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
 };
 
  
